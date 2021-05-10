97fa4b940215fd6edb5b5ac5576426efad04b9eb
Dreamycqy <1316152943@qq.com>
2021-05-10 09:59:43 +0800

    fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 7395
+const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use(express.static('public'))
